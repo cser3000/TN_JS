85d7acf076493e08a897068d16d9d118e2482629
Иван <69261519+cser3000@users.noreply.github.com>
2023-05-19 20:19:20 +0300

    Add files via upload

---
+++ task2/script.js
@@ -0,0 +1,16 @@
+
+function isPrime(a) {
+    if (a % 2 === 0) {
+        return false;
+    }
+    for (i = 3; i <= a / 2; i += 2) {
+        if (a % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+let a = prompt("введите число");
+
+alert(isPrime(+a));
