1b2138de8b3f618d928f3b9d7cb25eb6262b8c38
Иван <69261519+cser3000@users.noreply.github.com>
2023-06-07 15:28:29 +0300

    Add files via upload

--- task4/script.js
+++ task4/script.js
@@ -1,13 +1,18 @@
 
 array = [false, 1, 0, 1, 2, 0, 1, 3, "a"];
-function moveZero (array) {
+//function moveZero (array) {
+//
+//    for (let i = 0; i < array.length; i++) {
+//        if (array[i] === 0) {
+//            array.splice(i, 1);
+//            array.push(0);
+//        }
+//    }
+//}
 
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === 0) {
-            array.splice(i, 1);
-            array.push(0);
-        }
-    }
+function moveZero (array) {
+    array.reverse();
+    return array.sort( (a) => {if(a !== 0) return -1} );
 }
 
 alert(array);
@@ -15,3 +20,4 @@ alert(array);
 moveZero(array);
 
 alert(array);
+
